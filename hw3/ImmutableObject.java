f45d7a17b5f6caebcb86ce3a8107eef93841c34f
mdnayeem45 <143674810+mdnayeem45@users.noreply.github.com>
2023-09-20 11:37:41 -0400

    Add files via upload

--- hw3/ImmutableObject.java
+++ hw3/ImmutableObject.java
@@ -9,33 +9,57 @@ package com.mycompany.immutableobject;
  *
  * @author mdazizulhakimnayeem
  */
+import java.util.Scanner;
 public class ImmutableObject {
-
-    public static void main(String[] args) {
-        float fahrenheit;
-        float centigrade;
-
-        fahrenheit = 98.6f;
-
-        centigrade = (5.0f / 9.0f) * (fahrenheit - 32.0f);
-
-        System.out.println("Fahrenheit: " + fahrenheit);
-        System.out.println("Centigrade: " + centigrade);
-
-        System.out.println("Centigrade values from 0.0 to 40.0:");
-        for (float i = 0.0f; i <= 40.0f; i += 4.0f) {
-            float c = (5.0f / 9.0f) * (i - 32.0f);
-            System.out.println("Fahrenheit: " + i + " Centigrade: " + c);
-        }
-
-        System.out.println("Centigrade values from 0.0 to 40.0 (using while loop):");
-        float i = 0.0f;
-        while (i <= 40.0f) {
-            float c = (5.0f / 9.0f) * (i - 32.0f);
-            System.out.println("Fahrenheit: " + i + " Centigrade: " + c);
-            i += 4.0f;
+        public static void main(String[] args) {
+        // Step 2: Declare a 2D array containing two arrays
+        String[][] myArray = {
+            {"BMW", "Ferrari", "Lambo"},
+            {"pizza", "burger", "dumpling"}
+        };
+        
+        // Step 3: Declare two String variables
+        String isin;
+        String inputwords;
+        
+        // Step 4: Print out the sentence
+        System.out.println("Enter a word: ");
+        
+        // Step 5: Read input from the user
+        Scanner scanner = new Scanner(System.in);
+        inputwords = scanner.nextLine();
+        
+        // Check if the input is "STOP" to terminate the program
+        if (inputwords.equalsIgnoreCase("STOP")) {
+            StopChecking();
+        } else {
+            // Check if the input is in the 2D array
+            isin = checkInputInArray(inputwords, myArray);
+            if (isin.equals("True")) {
+                System.out.println(inputwords + " is in the 2D array");
+            } else {
+                System.out.println(inputwords + " is not in the 2D array");
+            }
         }
     }
+    
+    // Step 1: Method to stop checking
+    public static void StopChecking() {
+        System.out.println("Terminating the program.");
+        System.exit(0);
+    }
+    
+    // Method to check if the input is in the 2D array
+    public static String checkInputInArray(String input, String[][] array) {
+        for (String[] subArray : array) {
+            for (String item : subArray) {
+                if (item.equalsIgnoreCase(input)) {
+                    return "True";
+                }
+            }
+        }
+        return "False";
+    }
 }
-        
+       
     
